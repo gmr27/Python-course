380316a9e1d8baa98e31603366162fd09873a098
Goutham <gmredu27@gmail.com>
2021-12-21 18:31:38 +0530

    Completed section-3 step

---
+++ 1.Step/4.variables.py
@@ -0,0 +1,9 @@
+# Python allocates area of memory for us , and python also knows the type of variable 
+#Few rules for variable names
+#1.Must begin with letter or underscore
+#2.They can contain letters,numbers,underscore(cannot begin with number)
+#3.Case sensitive
+#4.Attached with Symbol =
+age=24
+print(age)
+print(type(age))
